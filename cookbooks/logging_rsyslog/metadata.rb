3f5ad75f45345d5584fe28cff431ae5c12ff74e9
Isel Fernandez <isel_77@hotmail.com>
2012-11-05 13:56:36 -0500

    282 - adding metadata to logging_rsyslog

---
+++ cookbooks/logging_rsyslog/metadata.rb
@@ -0,0 +1,12 @@
+maintainer       "Cloud Infrastructure"
+maintainer_email "csf@ultimatesoftware.com"
+license          "our license"
+description      "Deploys the UGF software to the environment"
+long_description ""
+version          "0.0.1"
+
+# supports "centos", "~> 5.8", "~> 6"
+# supports "redhat", "~> 5.8"
+# supports "ubuntu", "~> 10.04", "~> 12.04"
+
+depends "logging"
