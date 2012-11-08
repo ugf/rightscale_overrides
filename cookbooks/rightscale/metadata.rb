37ed85876d47e1c1a11683635782f8a4cde0010a
Robert Rehberg <robertrehberg@bellsouth.net>
2012-11-08 13:42:56 -0500

    AZ-299: Overriding monitoring.rb issues when included on a windows server

---
+++ cookbooks/rightscale/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "RightScale, Inc."
+maintainer_email "support@rightscale.com"
+license          "Copyright RightScale, Inc. All rights reserved."
+description      "RightScale Cookbooks"
+version          "13.2.0"
+
+
